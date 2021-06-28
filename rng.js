0cd30c5c00e6024f273edaca6916ae619d60bc12
Alexander Lazaris <alex.lazaris617@gmail.com>
2021-06-29 07:30:10 +1000

    test

---
+++ rng.js
@@ -0,0 +1,20 @@
+var maxLength = 5;
+var generatedNumbers = [];
+var dups = 0;
+var rndInt = 0;
+
+function generate() {
+  while (generatedNumbers.length != maxLength) {
+    rndInt = Math.floor(Math.random() * maxLength) + 1;
+    if (!generatedNumbers.includes(rndInt)) {
+      console.log(`new number is ${rndInt}`);
+      generatedNumbers.push(rndInt);
+    } else {
+      console.log(`${rndInt} is already used, skipping`);
+      dups++;
+    }
+  }
+  console.table(generatedNumbers);
+  console.log(`Duplicates: ${dups}`);
+}
+generate();
