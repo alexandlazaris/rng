aeffa247f6861c41e0ee99f15b92c57a4e983b41
Alexander Lazaris <alex.lazaris617@gmail.com>
2021-06-29 23:39:42 +1000

    more features more tests

--- tests/rng.test.js
+++ tests/rng.test.js
@@ -1,6 +1,16 @@
 const { expect } = require("@jest/globals");
-const generate = require("../rng");
+const rng = require("../rng");
 
-test("Generate numbers array is not empty", () => {
-  expect(generate()).toHaveLength(5);
+test("Generate numbers array has length", () => {
+  expect(rng.generate()).toHaveLength(5);
+});
+
+test("Generate object size matches specified size", () => {
+  const genObjectsSize = rng.generateObjects(8).size;
+  expect(genObjectsSize).toBe(8);
+});
+
+test("Generate object is undefined if specified size is too small", () => {
+  const genObjectsSize = rng.generateObjects(0);
+  expect(genObjectsSize).toBeUndefined;
 });
